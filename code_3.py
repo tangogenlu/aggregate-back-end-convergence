a2587374e7b9492d9ce7692cd1a93caed95ac362
Геннадий <140153539+tangogenlu@users.noreply.github.com>
2024-05-13 19:23:30 +0300

    Create code_3.py

---
+++ code_3.py
@@ -0,0 +1,37 @@
+
+        }
+}
+        int[] data = new int[10];
+public class RandomDataGenerator {
+        for (int item : data) {
+            System.out.println("Random Number: " + item);
+
+        }
+        for (int i = 0; i < 10; i++) {
+    }
+    public static void main(String[] args) {
+            data[i] = (int) (Math.random() * 100) + 1;        for (int item : data) {
+        }
+        }
+            data[i] = (int) (Math.random() * 100) + 1;
+        for (int i = 0; i < 10; i++) {
+    }
+}
+public class RandomDataGenerator {
+            System.out.println("Random Number: " + item);
+    public static void main(String[] args) {
+        int[] data = new int[10];
+
+            System.out.println("Random Number: " + item);
+    }
+        for (int i = 0; i < 10; i++) {
+    public static void main(String[] args) {
+
+        }
+        for (int item : data) {
+public class RandomDataGenerator {
+}
+
+        }
+        int[] data = new int[10];
+            data[i] = (int) (Math.random() * 100) + 1;
